191d73a075ed276e1025042053feecf90cacfa9a
Marcos Silva <marcos.silva79@gmail.com>
2019-01-31 10:37:20 -0700

    Addes ResearchRestaurant Page

---
+++ FoodApp/RestaurantSearch.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RestaurantSearch : ContentPage
+	{
+		public RestaurantSearch ()
+		{
+			InitializeComponent ();
+            FoodTypePicker.Items.Add("American Food");
+            FoodTypePicker.Items.Add("Brazilian");
+            FoodTypePicker.Items.Add("Chinese");
+            FoodTypePicker.Items.Add("Peruvian");
+
+        }
+
+        private void FoodTypePicker_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+           var name =  FoodTypePicker.Items[FoodTypePicker.SelectedIndex];
+
+            DisplayAlert(name, "Selected value", "OK");
+        }
+
+    }
+}
