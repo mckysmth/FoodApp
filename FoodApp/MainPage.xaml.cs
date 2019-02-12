a067772470381116311e71926db2ed8ce73a376f
Daniel <danielaranibaraz@gmail.com>
2019-02-12 13:29:53 -0700

    MainPageBtn
    Buttons on main page created to take to desired pages.

--- FoodApp/MainPage.xaml.cs
+++ FoodApp/MainPage.xaml.cs
@@ -14,11 +14,8 @@ namespace FoodApp
         public MainPage()
         {
             InitializeComponent();
-<<<<<<< HEAD
-            var collection = Client.GetUserCollection(); 
-=======
-            Client.GetUserCollection().InsertOne(new User()); 
->>>>>>> 7b08b738530608b0aaafa5a09d5bb2010236c4a1
+          //  Client.GetUserCollection().InsertOne(new User()); 
+
         }
     }
 }
