21f4b4b11c4bc06b4051854dbb088034ee83835a
Alexander Mestre <44935239+AlexanderMestre@users.noreply.github.com>
2019-02-18 22:10:47 -0700

    Validation for sign up
    Validation for passwords

--- FoodApp/Model/ValidatorExtensions.cs
+++ FoodApp/Model/ValidatorExtensions.cs
@@ -12,5 +12,24 @@ namespace FoodApp.Model
             Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
             return regex.IsMatch(s);
         }
+
+        public static bool ArePasswordsEqual( String a, String b)
+        {
+            bool flag;
+            if (a.Equals(b))
+            {
+                flag = true;
+            }
+            else flag = false;
+
+            return flag;
+
+        }
+
+
     }
+
+    
+
+
 }
